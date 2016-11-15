ef304291cafd9e99861b6cfb7212e2e528958750
roy <roy@drty.co.il>
2016-11-15 20:50:29 +0200

    assigment completed!

---
+++ assigment1/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('Lunchapp', [])
+.controller('LunchController', LunchController);
+
+LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  $scope.color = "primary";
+  $scope.string = "";
+
+  $scope.checkTooMuch = function () {
+    var items = $scope.string.split(',');
+    var itemsCount = items.length;
+    switch (true) {
+      case (itemsCount == 1):
+        $scope.message = "Please enter data first";
+        $scope.color = "danger";
+      break;
+      case (itemsCount <= 3):
+        $scope.message = "Enjoy!";
+        $scope.color = "success";
+      break;
+      default:
+        $scope.message = "Too much!";
+        $scope.color = "danger";
+    }
+  };
+}
+
+})();
